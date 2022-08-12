7a6ae4a80f43e892d3b2449d35dbbe94005673a9
aliceyhan <cyhn_666@hotmail.com>
2022-08-11 18:14:14 -0700

    code running

---
+++ src/Java_Answers/Answer_1.java
@@ -0,0 +1,20 @@
+package Java_Answers;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Answer_1 {
+    public static void main(String[] args) {
+        // 1-8-9-2-6-6-1-3-5-5
+       ArrayList<Integer>numbers=new ArrayList<Integer>(Arrays.asList(1,8,9,2,6,6,1,3,5,5));
+       ArrayList<Integer>soretedNumbers=new ArrayList<>();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (!soretedNumbers.contains(numbers.get(i))){
+                soretedNumbers.add(numbers.get(i));
+            }
+        }
+
+
+       System.out.println(soretedNumbers);
+    }
+}
