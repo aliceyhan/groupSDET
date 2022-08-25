9f1a69b10670ef61647bc14412cdff8701952a4b
Mazlum Yuksel <mazlumyuksel2121@gmail.com>
2022-08-24 19:02:10 -0700

    Interview Questions

---
+++ src/InterviewQuestionsByMazlum/InterviewQuestions.java
@@ -0,0 +1,64 @@
+package InterviewQuestionsByMazlum;
+
+public class InterviewQuestions {
+//    What are different types of Automation Testing?
+//
+//    Unit tests (Explain these test cases and when we need them?)
+//    Functional Tests
+//    GUI Tests
+//
+//    What is a Test Automation Framework?
+//
+//    A Framework is a set of the structure of the entire automation suite. It is also a guideline, which if followed can result in a structure which is easy to maintain and enhance.
+//
+//    What are differences between open source tools, vendor tools & in-house tools in automation testing?
+//    Open Source Tools
+//    Free tools for frameworks and applications
+//    Vendor Tools
+//    Developed by companies, they come with licenses
+//    In-House Tools
+//    Tools built by companies for their own use
+//
+//    What are the points that are covered while planning the phase of automation?
+//
+//    During planning phase of automations things which must be taken in concern are
+//    Selection the “right” Automation tool
+//    Selection Automation Framework if any
+//    List of In scope and out of scope items for automation
+//    Test Environment Setup
+//    Preparing Grant Chart of Project timelines for test script development & execution
+//    Identify Test Deliverables
+//
+//
+//
+//
+//    What do you include in a bug ticket?
+//
+//    Title;  The title should provide a quick description of the bug. Assigning an ID to the bug also helps to make identification easier.
+//    Environment:  A bug can appear in a particular environment and not others. For example, a bug appears when running the website on Firefox, or an app malfunction only when running on an iPhone X. These bugs can only be identified with cross browser testing or cross device tests. When reporting the bug, QAs must specify if the bug is observed in one or more specific environments.
+//    Steps to reproduce a Bug: Number the steps clearly from first to last so that the developers can quickly and exactly follow them to see the bug for themselves.
+//    Expected Result: How is the software supposed to work, with regard to the particular area in which the bug appears? The developer needs to know what the requirement is, in order to gauge the extent to which the bug is disrupting the user experience. Describe the ideal end-user scenario, and try to offer as much detail as possible. Don’t just leave it at “the app is crashing, and it shouldn’t.
+//    Actual Result:  Detail what the bug is actually doing, and how it is a distortion of the expected result. (Elaborate on the issue, Is the software crashing?,Is it simply pausing in action?, Does an error appear?,Or is it simply unresponsive? Specificity in this section will be most helpful to developers. Emphasize distinctly on what is going wrong. Provide additional details so that they can start investigating the issue with all variables in mind. For example:“Link does not lead to the expected page. It shows a 404 error.”
+//    Visual Proof (screenshots, videos, text) of Bug: Screenshots, videos of log files must be attached to clearly depict the occurrence of the bug. Depending on the nature of the bug, the developer may need video, text, and images.
+//    Severity/Priority: Every bug must be assigned a level of severity and corresponding priority. This reveals the extent to which the bug affects the system, and in turn, how quickly it needs to be fixed.
+//
+//
+//    How to reverse a String? Can you share your screen and show an  explanation?
+//
+//    What are the OOPs concepts?
+//
+//    OOPs concepts include:
+//    Inheritance
+//            Encapsulation
+//    Polymorphism
+//            Abstraction
+//    Interface
+//
+//    Difference between Array and Array List?
+//
+//    What is the final keyword in Java?
+//    Final variable: Once a variable is declared as final, then the value of the variable could not be changed. It is like a constant.
+//    What is Inheritance?
+//    Inheritance means one class can extend to another class. So that the codes can be reused from one class to another class. The existing class is known as the Super class whereas the derived class is known as a subclass.
+
+}
